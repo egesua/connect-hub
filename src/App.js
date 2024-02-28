6334f997378360db68c010530c75ddb9acf212f5
egesua <egesu06@gmail.com>
2024-02-28 20:37:53 +0300

    components are created and first be connection created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import User from "./components/User/User";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/users/:userId" element={<User />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "../Post/Post";
+
+function Home() {
+
+  return (
+    <div>
+      Home
+      <Post></Post>
+    </div>
+  )
+};
+
+export default Home;
