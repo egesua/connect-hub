ea7d973b1b60107485e79f877534002acb9cbd48
egesua <egesu06@gmail.com>
2024-03-06 20:49:02 +0300

    register button added.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,25 @@
-import { FormControl, Input, InputLabel } from "@mui/material";
-import React from "react"
+import { Button, FormControl, Input, InputLabel } from "@mui/material";
+import React from "react";
 
 function Auth() {
   return (
     <FormControl>
-        <InputLabel>Username</InputLabel>
-        <Input />
-        <InputLabel>Password</InputLabel>
-        <Input />
+      <InputLabel>Username</InputLabel>
+      <Input />
+      <InputLabel style={{ top: 80 }}>Password</InputLabel>
+      <Input style={{ top: 40 }} />
+      <Button
+        variant="contained"
+        style={{
+          marginTop: 60,
+          background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%",
+          color: "white",
+        }}
+      >
+        Register
+      </Button>
     </FormControl>
-  )
-};
+  );
+}
 
 export default Auth;
